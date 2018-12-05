37bbdcadfa9995293f149d6fc937e2ca81cad088
iFanie <fanieveizis@gmail.com>
2018-12-06 00:15:04 +0200

    Override generation path in compiler

--- interventioncompiler/src/main/java/com/izikode/izilib/interventioncompiler/InterventionCompiler.kt
+++ interventioncompiler/src/main/java/com/izikode/izilib/interventioncompiler/InterventionCompiler.kt
@@ -3,17 +3,29 @@ package com.izikode.izilib.interventioncompiler
 import com.izikode.izilib.basekotlincompiler.BaseKotlinCompiler
 import com.izikode.izilib.basekotlincompiler.component.CompilationRound
 import com.izikode.izilib.intervention.Intervene
+import java.io.File
 import kotlin.reflect.KClass
 
 class InterventionCompiler : BaseKotlinCompiler() {
 
     override val processes: Array<KClass<out Any>> = arrayOf( Intervene::class )
 
+    override fun getGeneratedSourceDirectory(options: Map<String, String>) =
+        options[PROJECT_ARGUMENT]?.let { File("$it/$GENERATED_DIRECTORY") } ?:
+            super.getGeneratedSourceDirectory(options)
+
     override fun handle(compilationRound: CompilationRound) {
 
     }
 
     override fun finally() {
+
+    }
+
+    companion object {
+
+        const val PROJECT_ARGUMENT = "interventionsModuleDir"
+        const val GENERATED_DIRECTORY = "build/generated/source/kaptKotlin"
 
     }
 
