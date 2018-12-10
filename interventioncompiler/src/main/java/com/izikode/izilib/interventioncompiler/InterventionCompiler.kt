3e6dd19d5f86f95fb9cbdd7f005fbcd4f8c71f27
iFanie <fanieveizis@gmail.com>
2018-12-11 00:48:59 +0200

    Update compiler to new builders

--- interventioncompiler/src/main/java/com/izikode/izilib/interventioncompiler/InterventionCompiler.kt
+++ interventioncompiler/src/main/java/com/izikode/izilib/interventioncompiler/InterventionCompiler.kt
@@ -12,14 +12,34 @@ class InterventionCompiler : BaseKotlinCompiler() {
 
     override fun getGeneratedSourceDirectory(options: Map<String, String>) =
         options[PROJECT_ARGUMENT]?.let { File("$it/$GENERATED_DIRECTORY") } ?:
-            super.getGeneratedSourceDirectory(options)
+        super.getGeneratedSourceDirectory(options)
+
+    private val interventionBuilders = mutableListOf<InterventionBuilder>()
 
     override fun handle(compilationRound: CompilationRound) {
+        compilationRound.functionsWith(Intervene::class).forEach { interventionFunction ->
+            interventionBuilders.add(InterventionBuilder(
 
+                interventionFunction.annotation.name,
+                interventionFunction.annotation.warnAgainst,
+                interventionFunction.toString(),
+                interventionFunction.annotation.priority.value,
+                interventionFunction.annotation.type == Intervene.Type.ERROR
+
+            ))
+        }
     }
 
     override fun finally() {
+        compilationUtilities.classGenerator.apply {
+            interventionBuilders.forEach { interventionBuilder ->
+                generate(interventionBuilder)
+            }
 
+            generate(RegistryBuilder(Array(interventionBuilders.size) { index ->
+                interventionBuilders[index].issue
+            }))
+        }
     }
 
     companion object {
